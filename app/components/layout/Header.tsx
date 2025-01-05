65a8eb2620a59887d7e144995582512093ca0789
david <colinsinclairsmith2@gmail.com>
2025-01-05 23:59:08 +0000

    fix bug

--- app/components/layout/Header.tsx
+++ app/components/layout/Header.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function header() {
   return (
     <header className="p-7 absolute top-0 right-0">
-      <Link
+      {/* <Link
         href="/contact-me"
         className="relative inline-flex h-12 overflow-hidden rounded-full p-[1px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50"
       >
@@ -11,7 +11,7 @@ export default function header() {
         <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-slate-950 px-3 py-1 text-sm font-medium text-white backdrop-blur-3xl">
           Get in touch
         </span>
-      </Link>
+      </Link> */}
     </header>
   );
 }
